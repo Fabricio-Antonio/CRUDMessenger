ec6065fc00eb556d71fb8c0040a7f14460a1e2fc
Fabrício Antõnio <fabricio.ss2117@gmail.com>
2025-03-24 22:18:08 -0300

    🧠: learn: learn about atomatic service

--- src/automatic-concepts/automatic-concepts.controller.ts
+++ src/automatic-concepts/automatic-concepts.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { AutomaticConceptsService } from './automatic-concepts.service';
 
 @Controller('automatic-concepts')
 export class AutomaticConceptsController {
+    constructor(private readonly automaticConceptsService: AutomaticConceptsService) {}
+
     @Get()
     home(): string {
-        return 'automatic-concepts';
+        return this.automaticConceptsService.getHome();
     }
 }
---
+++ src/automatic-concepts/automatic-concepts.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AutomaticConceptsService {
+    getHome() {
+        return 'automatic-concepts (AutomaticConceptsService)';
+    }
+}
