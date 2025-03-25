8a02ad9c4454496cf4a004ff58a048e08fcf664a
Fabrício Antõnio <fabricio.ss2117@gmail.com>
2025-03-24 22:17:34 -0300

    🧠: learn: learn about manual service

--- src/manual-concepts/manual-concepts.controller.ts
+++ src/manual-concepts/manual-concepts.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ManualConceptsService } from './manual-concepts.service';
 
 @Controller('manual-concepts')
 export class ManualConceptsController {
+    constructor(private readonly manualConceptsService: ManualConceptsService) {}
+
     @Get()
     home(): string {
-    return 'conceitos-manual';
+    return this.manualConceptsService.HomeSolution();
     }
 }
---
+++ src/manual-concepts/manual-concepts.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class ManualConceptsService {
+    HomeSolution(): string {
+        return 'Home of manual concepts solution'
+    }
+}
