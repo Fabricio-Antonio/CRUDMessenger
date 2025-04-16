d26fa647f114096a839984cc47713433eb5c54b6
Fabrício Antõnio <fabricio.ss2117@gmail.com>
2025-04-15 21:08:16 -0300

    🧠: learn: learning about validation

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,5 +1,21 @@
+import { IsString, IsNotEmpty, MinLength, MaxLength } from 'class-validator'
+
 export class CreateNoteDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(5)
+    @MaxLength(255)
     readonly text: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(50)
     readonly to: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(50)
     readonly from: string;
 }
