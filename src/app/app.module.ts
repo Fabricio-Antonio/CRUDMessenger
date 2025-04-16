9611bb1fd0595ac46fa9f36a05a954bffbaf5b1e
Fabrício Antõnio <fabricio.ss2117@gmail.com>
2025-04-16 14:34:41 -0300

    :wrench: chore: setup DB infos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesModule } from 'src/notes/notes.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [NotesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // Torna acessível em toda a aplicação
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: parseInt(configService.get('DB_PORT', '5432')),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // NEVER must be true in prod
+      }),
+    }),
+    NotesModule
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
