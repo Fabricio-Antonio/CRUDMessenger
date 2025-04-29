61a7d011fae4a3be3b1cbb093c9c45bb6e05bc39
Fabrício Antõnio <fabricio.ss2117@gmail.com>
2025-04-29 15:21:24 -0300

    :broom: cleanup: cleaning code

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -7,39 +7,23 @@ import {
   Patch,
   Delete,
   Query,
-  Inject,
 } from '@nestjs/common';
 import { NotesService } from './notes.service';
 import { CreateNoteDto } from './dto/create-note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { NotesUtils } from './notes.utils';
-import { RegexProtocol } from 'src/common/regex/regex.protocol';
-import {
-  ONLY_LOWERCASE_LETTERS,
-  REMOVE_SPACES,
-  SERVER_NAME,
-} from 'src/notes/notes.constants';
 
 @Controller('notes')
 export class NotesController {
   constructor(
-    @Inject(SERVER_NAME)
-    private readonly serverName: string,
     private readonly notesServices: NotesService,
     private readonly notesUtils: NotesUtils,
-    @Inject(REMOVE_SPACES)
-    private readonly removeSpacesRegex: RegexProtocol,
-    @Inject(ONLY_LOWERCASE_LETTERS)
-    private readonly onlyLowercaseLattersRagex: RegexProtocol,
   ) {}
 
   // Find all notes
   @Get()
   async findAll(@Query() paginationDto: PaginationDto) {
-    console.log(this.removeSpacesRegex.execute(this.serverName));
-    console.log(this.onlyLowercaseLattersRagex.execute(this.serverName));
-    console.log(this.serverName);
     return this.notesServices.findAll(paginationDto);
 
     const notes = await this.notesServices.findAll();
--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -6,32 +6,10 @@ import { Note } from './entities/note.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PeopleModule } from 'src/people/people.module';
 import { NotesUtils } from './notes.utils';
-import { RemoveSpacesRegex } from 'src/common/regex/remove-spacees.regex';
-import { OnlyLowerCaseLettersRegex } from 'src/common/regex/only-lowercase-latters.regex';
-import {
-  ONLY_LOWERCASE_LETTERS,
-  REMOVE_SPACES,
-  SERVER_NAME,
-} from './notes.constants';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Note]), PeopleModule],
   controllers: [NotesController],
-  providers: [
-    NotesService,
-    NotesUtils,
-    {
-      provide: SERVER_NAME,
-      useValue: 'My Name Is NestJS',
-    },
-    {
-      provide: ONLY_LOWERCASE_LETTERS,
-      useClass: OnlyLowerCaseLettersRegex,
-    },
-    {
-      provide: REMOVE_SPACES,
-      useClass: RemoveSpacesRegex,
-    },
-  ],
+  providers: [NotesService, NotesUtils],
 })
 export class NotesModule {}
