86076981cea596cdfa1cebdbf9a6f84408c96188
Fabrício Antõnio <fabricio.ss2117@gmail.com>
2025-06-02 02:40:53 -0300

    :test_tube: test: add test to auth.controller

---
+++ src/auth/auth.controller.spec.ts
@@ -0,0 +1,71 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { RefreshTokenDto } from './dto/refresh-toke-token.dto';
+
+describe('AuthController', () => {
+  let authController: AuthController;
+  let authService: AuthService;
+
+  const mockAuthService = {
+    login: jest.fn(),
+    refreshTokens: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
+    }).compile();
+
+    authController = moduleRef.get<AuthController>(AuthController);
+    authService = moduleRef.get<AuthService>(AuthService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('login', () => {
+    it('should return access and refresh tokens when login is successful', async () => {
+      const loginDto: LoginDto = {
+        email: 'test@example.com',
+        password: 'securepassword',
+      };
+
+      const result = {
+        accessToken: 'access_token_mock',
+        refreshToken: 'refresh_token_mock',
+      };
+
+      mockAuthService.login.mockResolvedValue(result);
+
+      expect(await authController.login(loginDto)).toEqual(result);
+      expect(mockAuthService.login).toHaveBeenCalledWith(loginDto);
+    });
+  });
+
+  describe('refreshTokens', () => {
+    it('should return new tokens when refresh token is valid', async () => {
+      const refreshTokenDto: RefreshTokenDto = {
+        refreshToken: 'valid_refresh_token',
+      };
+
+      const result = {
+        accessToken: 'new_access_token',
+        refreshToken: 'new_refresh_token',
+      };
+
+      mockAuthService.refreshTokens.mockResolvedValue(result);
+
+      expect(await authController.refreshTokens(refreshTokenDto)).toEqual(result);
+      expect(mockAuthService.refreshTokens).toHaveBeenCalledWith(refreshTokenDto);
+    });
+  });
+});
