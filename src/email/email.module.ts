23eb70d2adc30967f55431382aed8675fce7d86c
Fabrício Antõnio <fabricio.ss2117@gmail.com>
2025-06-03 19:48:30 -0300

    :broom: cleanup: removing duplicate code

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 
 @Module({
-  providers: [EmailService, EmailService],
+  providers: [EmailService],
   exports: [EmailService],
 })
 export class EmailModule {}
