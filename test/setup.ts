cf4fc36f6efac391f55ba90170792060640953b9
Fabrício Antõnio <fabricio.ss2117@gmail.com>
2025-06-06 18:47:25 -0300

    :recycle: refactor: configure global email service mock to skip email.e2e-spec.ts

--- test/setup.ts
+++ test/setup.ts
@@ -1,25 +1,14 @@
-import { Test } from '@nestjs/testing';
 import { EmailService } from '../src/email/email.service';
 
-// Mock do EmailService
-class MockEmailService {
-  async sendEmail() {
-    return Promise.resolve();
+beforeAll(() => {
+  const testFile = expect.getState().testPath;
+  if (!testFile?.includes('email.e2e-spec.ts')) {
+    jest
+      .spyOn(EmailService.prototype, 'sendEmail')
+      .mockImplementation(() => Promise.resolve());
   }
-}
+});
 
-beforeAll(async () => {
-  const moduleRef = await Test.createTestingModule({
-    providers: [
-      {
-        provide: EmailService,
-        useClass: MockEmailService,
-      },
-    ],
-  }).compile();
-
-  const emailService = moduleRef.get<EmailService>(EmailService);
-  jest
-    .spyOn(emailService, 'sendEmail')
-    .mockImplementation(() => Promise.resolve());
+afterEach(() => {
+  jest.clearAllMocks();
 });
